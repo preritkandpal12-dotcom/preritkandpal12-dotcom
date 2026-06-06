bb74c468f85dbc2a5f63884b48e63eb4969ca43e
preritkandpal12-dotcom <preritkandpal12@gmail.com>
2026-06-07 00:25:08 +0530

    Delete import pandas as pd.py

--- import pandas as pd.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd 
-df = pd.read_csv('weather_by_cities.csv')
-print(df.head())
-max_temp = df['tempreature'].max()
-print("The maximun tempreature recorded was:",max_temp) 
-ny_weather = df[df['city'] == 'new york']
-print("Here is the weather data just for new york:", ny_weather) 
-print(ny_weather)
